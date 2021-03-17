e53e387b4a5b47c99de3b6a54c612383b6608e97
Casper G <caspergustavsson5@gmail.com>
2021-03-17 18:45:27 +0100

    fade in fade out
    started fadescript

--- New Unity Project/Assets/Scripts/FadeInOutLevels.cs
+++ New Unity Project/Assets/Scripts/FadeInOutLevels.cs
@@ -6,9 +6,13 @@ public class FadeInOutLevels : MonoBehaviour
 {
 
     public List<GameObject> levels;
-    public int stage;
-    public float fadeSpeed;
-    public bool next;
+    public int stage = 0;
+    public float fadeSpeed = 1;
+    private bool next = false;
+    private bool fading = false;
+    private bool fadingIn = false;
+    List<Material> mats = new List<Material>();
+
     // Start is called before the first frame update
     void Start()
     {
@@ -18,26 +22,49 @@ public class FadeInOutLevels : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        
+        if(next)
+        {
+            if(!fading)
+            {
+                mats.Clear();
+                int numChild = levels[stage].transform.childCount;
+                for(int i = 0; i < numChild; i++)
+                {
+                    GameObject child = levels[stage].transform.GetChild(i).gameObject;
+                    foreach(Material mat in child.GetComponent<Renderer>().materials)
+                    mats.Add(mat);
+                }
+                fading = true;
+            }
+            if(fading)
+            {
+                for (int i = 0; i < mats.Count; i++)
+                {
+                    Color ObjColor = mats[i].color;
+                    float fadeAmount = ObjColor.a + (fadingIn == true ? fadeSpeed : -fadeSpeed);
+
+                    ObjColor = new Color(ObjColor.r, ObjColor.g, ObjColor.b, fadeAmount);
+                    mats[i].color = ObjColor;
+                    if(fadingIn && fadeAmount >= 255)
+                    {
+                        fadingIn = false;
+                        next = false;
+                    }
+                    if(!fadingIn && fadeAmount <= 0)
+                    {
+                        fadingIn = true;
+                        fading = false;
+                        stage++;
+                    }
+                }
+            }
+
+        }
     }
 
-    void LoadNextStage()
+    void LoadStage()
     {
-        int numChild = levels[stage].transform.childCount;
-        List<Material> mats = new List<Material>();
-        for(int i = 0; i < numChild; i++)
-        {
-            GameObject child = levels[stage].transform.GetChild(i).gameObject;
-            foreach(Material mat in child.GetComponent<Renderer>().materials)
-            mats.Add(mat);
-        }
-        for (int i = 0; i < mats.Count; i++)
-        {
-            Color ObjColor = mats[i].color;
-            float fadeAmount = ObjColor.a - fadeSpeed;
-
-            ObjColor = new Color(ObjColor.r, ObjColor.g, ObjColor.b, fadeAmount);
-            mats[i].color = ObjColor;
-        }
+        next = true;
+        fadingIn = stage == 0 ? true : false;
     }
 }

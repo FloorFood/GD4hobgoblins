051820f4db5d51e68912c0f9dd09f769aad31d68
Nicolas Hammar <nico.hammar@gmail.com>
2021-03-21 23:02:24 +0100

    Added end screen functionality and made the happy ghost sounds play when you get it.

---
+++ New Unity Project/Assets/EndState.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndState : MonoBehaviour
+{
+    public GameObject fin;
+    public GameObject finalArea;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        FindObjectOfType<AudioManager>().Play("OldGhostHappy");
+        FindObjectOfType<AudioManager>().Play("LonelyGhostHappy");
+        FindObjectOfType<AudioManager>().Play("MellowGhostHappy");
+        fin.SetActive(true);
+        finalArea.SetActive(false);
+    }
+}
